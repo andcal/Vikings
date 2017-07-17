661badd376d415e4249ee267f432cd5bad3a13d4
GuillermoMTorres <Shadowete2@gmail.com>
2017-07-17 18:36:06 +0200

    Parte segunda

--- vikings.js
+++ vikings.js
@@ -1,48 +1,68 @@
 //Clase Vikingo
-var Viking = function(name, health, strength) {
+var Viking = function(name, currHealth, strength) {
 	this.name = name;
-	this.health = health;
+	this.currHealth = currHealth;
 	this.strength = strength;
+
 };
 
-var vik1 = new Viking('Odin', 100, 61);
+var vik1 = new Viking('Odin', 100, 15);
 var vik2 = new Viking('Thor', 120, 8);
 
 //Llama dos vikingo a la lucha
  function pitFight (viking1, viking2) {
  	var turn = 1;
- 	var maxH1 = viking1.health * 0.15;
- 	var maxH2 = viking2.health * 0.15;
- 	var lowHealth = false;
+ 	var start = Math.random();
+
  	do {
- 		console.log('Turno ' + turn);
- 		//Acción vikingo 1
- 		viking2.health = viking2.health - viking1.strength;
- 		console.log(viking2.name + " ha recibido " + viking1.strength + " de daño y le queda " + viking2.health + " de vida.");
- 		//Acción vikingo 2
- 		viking1.health = viking1.health - viking2.strength;
- 		console.log(viking1.name + " ha recibido " + viking2.strength + " de daño y le queda " + viking1.health + " de vida.");
- 		if(viking1.health < maxH1 || viking2.health < maxH2){
- 			lowHealth = true;
- 		}
- 		if(viking1.health <= 0) {
- 			viking1.health = 1;
- 		}
- 		if(viking2.health <= 0) {
- 			viking2.health = 1;
- 		}
- 		//Nuevo turno
+ 		console.log("Turno " + turn);
+ 		if(start<0.5){
+	 		if(viking2.currHealth > viking1.strength){
+	 			viking2.currHealth = viking2.currHealth - viking1.strength;
+	 			console.log(viking2.name + " le queda " + viking2.currHealth + " de vida tras recibir " + viking1.strength + " de daño!");
+	 		}else{
+	 			console.log(viking1.name + " le perdona la vida al ver que " + viking2.name + " esta tambalándose")
+	 			console.log("¡" + viking1.name + " ha ganado el entrenamiento!")
+	 			return;
+	 		}
+	 		if(viking1.currHealth > viking2.strength){
+	 			viking1.currHealth = viking1.currHealth - viking2.strength;
+	 			console.log(viking1.name + " le queda " + viking1.currHealth + " tras recibir " + viking2.strength + " de daño!");
+	 		}else{
+	 			console.log(viking2.name + " le perdona la vida al ver que " + viking1.name + " esta tambalándose")
+	 			console.log("¡" + viking2.name + " ha ganado el entrenamiento!")
+	 			return;
+	 		}
+	 	}else{
+	 		if(viking1.currHealth > viking2.strength){
+	 			viking1.currHealth = viking1.currHealth - viking2.strength;
+	 			console.log(viking1.name + " le queda " + viking1.currHealth + " tras recibir " + viking2.strength + " de daño!");
+	 		}else{
+	 			console.log(viking2.name + " le perdona la vida al ver que " + viking1.name + " esta tambalándose")
+	 			console.log("¡" + viking2.name + " ha ganado el entrenamiento!")
+	 			return;
+	 		}
+	 		if(viking2.currHealth > viking1.strength){
+	 			viking2.currHealth = viking2.currHealth - viking1.strength;
+	 			console.log(viking2.name + " le queda " + viking2.currHealth + " de vida tras recibir " + viking1.strength + " de daño!");
+	 		}else{
+	 			console.log(viking1.name + " le perdona la vida al ver que " + viking2.name + " esta tambalándose")
+	 			console.log("¡" + viking1.name + " ha ganado el entrenamiento!")
+	 			return;
+	 		}
+	 	}
+	 	start = Math.random();
  		turn++;
-
  	}
- 	while (turn <= 5 && !lowHealth);
+ 	while (turn <= 5);
 
- 	//Ganador del entrenamiento
- 	console.log(viking1.name + " tiene " + viking1.health + " de vida.");
- 	console.log(viking2.name + " tiene " + viking2.health + " de vida.");
- 	if(viking1.health < viking2.health) {
- 		console.log("¡" + viking2.name + " ha ganado el entrenamiento!")
- 	} else {
- 		console.log("¡" + viking1.name + " ha ganado el entrenamiento!")
+ 	console.log("A " + viking1.name + " le queda " + viking1.currHealth + " de vida");
+ 	console.log("A " + viking2.name + " le queda " + viking2.currHealth + " de vida");
+
+ 	if(viking1.currHealth < viking2.currHealth){
+ 		console.log(viking2.name + " ha ganado");
+ 	}else{
+ 		console.log(viking1.name + " ha ganado");
  	}
  }
+
